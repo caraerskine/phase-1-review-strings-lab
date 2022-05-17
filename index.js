b4b422534f98e11a6604eedaf929c25dcbbfffd9
CaraErskine <caraerskine@gmail.com>
2022-05-17 09:48:40 -0400

    commit tree 686534377e9c25ce92706b1d40757865bc944ad0

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = "Max Johnson";
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
